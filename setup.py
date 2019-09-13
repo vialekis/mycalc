27e4b868a2c6bdebd01fec7c0d38dfb3e668f0fb
vialekis <vialekis@yandex.ru>
2019-09-13 13:59:19 +0600

    Add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mycalc",
+    version="0.0.1",
+    description="A small example package with some arithmetics",
+    packages=find_packages(),
+    include_package_data=True,
+    author="vik",
+    author_email="2702040@example.com",
+    url="https://github.com/vialekis/mycalc",
+)
