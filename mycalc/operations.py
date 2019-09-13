4a0fc4f9a1d8b86fd653a0935227aa3e0902c758
vialekis <vialekis@yandex.ru>
2019-09-13 13:51:07 +0600

    Inital commit

---
+++ mycalc/operations.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+def mul(a, b):
+    return a * b
+
+def sub(a, b):
+    return add(a, -b)
+
+def div(a, b):
+    return a / b
+
+def sqrt(x):
+    return x**(0.5)
